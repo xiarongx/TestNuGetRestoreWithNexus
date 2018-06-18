27a66bed4dee221384ad9be2f80663f1dc3c9641
xiarongx <xiarongx@uci.edu>
2018-06-18 13:45:57 -0400

    Second commit

---
+++ TEstNuGetRestore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ACReditPlus.TestLib1;
+namespace TestNuGetRestore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ReturnString returnString = new ReturnString();
+            Console.WriteLine(returnString.TimeAndLocation());
+            Console.Read();
+        }
+    }
+}
